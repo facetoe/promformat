3440a6e6040945050bfea8141b6f828cf0ead3d4
William Coe <willcoe@aiven.io>
2022-09-20 07:48:25 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="promformat",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "antlr4-python3-runtime>=4.10"

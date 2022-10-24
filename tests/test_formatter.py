452fdba5f5d3deed631b74623dcb0a41595182b5
Nick Farrell <nick.farrell@aiven.io>
2022-10-24 11:54:11 +1100

    Parametrise the unit tests
    This allows each test to run separately, and be named corresponding
    to location. Among other things it assists when running with
    `-f` a.k.a looponfail, meaning individual failing tests will be rerun
    alone.

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,10 +1,10 @@
 import pkg_resources
 import yaml
-
+import pytest
 from promformat.__main__ import format_query
 
 
-def test_formatter():
+def get_rules():
     rule_path = pkg_resources.resource_filename("tests.resources", "test-promql.yaml")
     with open(rule_path) as f:
         data = yaml.safe_load(f)
@@ -14,5 +14,21 @@ def test_formatter():
                     rules = exporter.get("rules")
                     if rules:
                         for rule in rules:
-                            res = format_query(rule["query"])
-                            assert res
+                            yield dict(
+                                group=group.get("name", "unnamed group"),
+                                service=service.get("name", "unnamed service"),
+                                exporter=exporter.get("name", "unnamed exporter"),
+                                rule=rule.get("name", "unnamed rule"),
+                                query=rule["query"],
+                            )
+
+
+@pytest.mark.parametrize(
+    ["query"],
+    [
+        pytest.param(rule["query"], id="{service}/{exporter}/{rule}".format(**rule))
+        for rule in get_rules()
+    ],
+)
+def test_formatter(query):
+    assert format_query(query)

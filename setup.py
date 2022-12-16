1e9a88efa8583e698d62a0ba0bc535cd8660ca3c
Adam Martin <89938352+aiven-amartin@users.noreply.github.com>
2022-12-16 17:37:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="promformat",
     version="0.0.4",
-    packages=find_packages(exclude="tests"),
+    packages=["promformat"],
     install_requires=["antlr4-python3-runtime>=4.10"],
     tests_require=["pytest"],
     setup_requires=["flake8", "black"],

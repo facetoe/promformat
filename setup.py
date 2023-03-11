047785237c6d0441ba16fbbb6f2f5eee9a5da909
William Coe <willcoe@aiven.io>
2023-03-11 21:10:07 +0800

    setup: bump version
    We now support comments and the formatter has been used in anger for
    months without too many issues, so bump the version to 1.0.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="promformat",
-    version="0.0.4",
+    version="1.0.0",
     packages=find_packages(exclude=["tests*"]),
     install_requires=["antlr4-python3-runtime>=4.10"],
     tests_require=["pytest", "PyYAML"],

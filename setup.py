e3d0971ec518105e0d38b8cd714070741a0b6cd8
William Coe <willcoe@aiven.io>
2023-03-11 22:42:45 +0800

    Bump Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="promformat",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(exclude=["tests*"]),
     install_requires=["antlr4-python3-runtime>=4.10"],
     tests_require=["pytest", "PyYAML"],

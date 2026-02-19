b610242f7d1c8526fc0d8355d2ca03443a1d29a6
Facetoe <facetoe@facetoe.com.au>
2026-02-19 16:44:30 +0800

    Actually run tests.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version="1.0.2",
     packages=find_packages(exclude=["tests*"]),
     install_requires=["antlr4-python3-runtime>=4.10"],
-    tests_require=["pytest", "PyYAML"],
-    setup_requires=["flake8", "black"],
+    tests_require=["pytest", "PyYAML", "setuptools"],
+    setup_requires=["flake8", "black", "setuptools"],
     entry_points={
         "console_scripts": [
             "promformat = promformat.__main__:main",
